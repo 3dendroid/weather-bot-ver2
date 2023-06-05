62066de49193ca07b7098857b383be86aba1b6da
GIGACHAD <denkingbad@gmail.com>
2023-06-05 17:11:40 +0700

    new version of weather bot

---
+++ handlers/users/start.py
@@ -0,0 +1,57 @@
+import datetime
+
+import requests
+from aiogram import types
+from aiogram.dispatcher.filters.builtin import CommandStart
+
+from data.config import OWM
+from loader import dp
+
+
+@dp.message_handler(CommandStart())
+async def bot_start(message: types.Message):
+    await message.answer(f"Привет, {message.from_user.full_name}!")
+    await message.answer("Напиши мне название города и я пришлю сводку погоды!")
+
+
+@dp.message_handler()
+async def get_weather_def(message: types.Message):
+    code_to_emoji = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
+    try:
+        r = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={OWM}&units=metric"
+        )
+        data = r.json()
+
+        city = data["name"]
+        cur_weather = data["main"]["temp"]
+
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_emoji:
+            wd = code_to_emoji[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
+
+        humidity = data["main"]["humidity"]
+        pressure = data["main"]["pressure"]
+        wind = data["wind"]["speed"]
+        sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+        sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+        length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+            data["sys"]["sunrise"])
+
+        await message.reply(f"Погода в городе: {city}\nТемпература: {cur_weather}C° {wd}\n"
+                            f"Влажность: {humidity}%\nДавление: {pressure} мм.рт.ст\nВетер: {wind} м/с\n"
+                            f"Восход солнца: {sunrise_timestamp}\nЗакат солнца: {sunset_timestamp}\nПродолжительность дня: {length_of_the_day}\n"
+                            )
+    except:
+        await message.reply("\U00002620 Проверьте название города \U00002620")
